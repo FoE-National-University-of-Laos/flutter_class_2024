8fff7b6ad3d3cd74aa3acff1ce8e42a26396046b
Savath Saypadith <mr.saypadith@gmail.com>
2024-05-07 19:36:29 +0700

    add container

--- ceit_2com_c2/lib/main_screen.dart
+++ ceit_2com_c2/lib/main_screen.dart
@@ -10,6 +10,49 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello, World!");
+    return Scaffold(
+      appBar: AppBar(title: Text("CEIT")),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        bottomRight: Radius.circular(32))),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
