5df9027b4c409e003abfcbf4258e8e36ffd13f76
Savath Saypadith <mr.saypadith@gmail.com>
2024-05-07 18:18:18 +0700

    add container

--- ceit_2com_c1/lib/main_screen.dart
+++ ceit_2com_c1/lib/main_screen.dart
@@ -10,6 +10,52 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("CEIT 2COM C1");
+    return Scaffold(
+      appBar: AppBar(
+        title: Icon(Icons.headphones),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 200,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(38),
+                      bottomRight: Radius.circular(38)),
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- ceit_master_g11/lib/views/detail_screen.dart
+++ ceit_master_g11/lib/views/detail_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:ceit_master_g11/views/third_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -10,22 +13,89 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  bool? isFavorite;
+  late http.Response data;
+  String title = "Detail Screen";
+
+  @override
+  void initState() {
+    isFavorite = true;
+    getData();
+    super.initState();
+  }
+
+  Future getData() async {
+    // get data from REST api using http
+    data = await http
+        .get(Uri.parse("https://jsonplaceholder.typicode.com/posts/2"));
+    var decodedResponse = jsonDecode(data.body) as Map;
+    var res = Uri.parse(decodedResponse['title'] as String);
+    setState(() {
+      title = res.toString();
+    });
+    return "Data";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Detail Screen"),
+        title: const Text("ລາຍລະອຽດ"),
       ),
       body: Center(
           child: Column(
         children: [
-          Text("Detail Screen"),
+          Text(title),
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                margin: const EdgeInsets.all(20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    "assets/images/testimg.jpeg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.red.withAlpha(80),
+                          borderRadius: BorderRadius.circular(18)),
+                      margin: const EdgeInsets.all(20),
+                      padding: const EdgeInsets.all(10),
+                      child: const Text("20,000 KIP",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)))),
+              Positioned(
+                  top: 20,
+                  right: 20,
+                  child: Container(
+                      margin: EdgeInsets.all(20),
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isFavorite = !isFavorite!;
+                          });
+                        },
+                        icon: Icon(Icons.favorite),
+                        color: isFavorite! ? Colors.red : Colors.grey,
+                      )))
+            ],
+          ),
           ElevatedButton(
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => ThirdScreen()));
             },
-            child: Text("Third Screen"),
+            child: const Text("ໜ້າຖັດໄປ",
+                style: TextStyle(fontFamily: 'phetsarath')),
           ),
         ],
       )),
