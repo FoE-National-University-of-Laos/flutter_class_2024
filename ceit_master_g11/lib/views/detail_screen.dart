45f3d6d5a2a571f754b173297bff6388e65833c9
Savath Saypadith <mr.saypadith@gmail.com>
2024-05-08 19:32:09 +0700

    add local storage package

--- ceit_master_g11/lib/views/detail_screen.dart
+++ ceit_master_g11/lib/views/detail_screen.dart
@@ -4,6 +4,8 @@ import 'package:ceit_master_g11/views/third_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -19,9 +21,27 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   void initState() {
-    isFavorite = true;
-    getData();
+    getDatafromLocal();
     super.initState();
+  }
+
+  Future getDatafromLocal() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? favorite = prefs.getBool("isFavorite");
+    if (favorite != null) {
+      setState(() {
+        isFavorite = favorite;
+      });
+    } else {
+      isFavorite = false;
+    }
+  }
+
+  // save data to local storage (shared preference)
+  void saveData(bool favorite) async {
+    // save data to local storage
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("isFavorite", favorite);
   }
 
   Future getData() async {
@@ -42,63 +62,92 @@ class _DetailScreenState extends State<DetailScreen> {
       appBar: AppBar(
         title: const Text("ລາຍລະອຽດ"),
       ),
-      body: Center(
-          child: Column(
-        children: [
-          Text(title),
-          Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                margin: const EdgeInsets.all(20),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                    "assets/images/testimg.jpeg",
-                    fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+          children: [
+            Text(title),
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  margin: const EdgeInsets.all(20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      "assets/images/testimg.jpeg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red.withAlpha(80),
+                            borderRadius: BorderRadius.circular(18)),
+                        margin: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(10),
+                        child: const Text("20,000 KIP",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)))),
+                Positioned(
+                    top: 20,
+                    right: 20,
+                    child: Container(
+                        margin: EdgeInsets.all(20),
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isFavorite = !isFavorite!;
+                              saveData(isFavorite!);
+                            });
+                          },
+                          icon: Icon(Icons.favorite),
+                          color: isFavorite != null && isFavorite!
+                              ? Colors.red
+                              : Colors.grey,
+                        )))
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: const Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            SizedBox(
+              height: 300,
+              child: PrettyQrView.data(
+                data: 'Savath Saypadith',
+                decoration: const PrettyQrDecoration(
+                  image: PrettyQrDecorationImage(
+                    image: AssetImage('assets/images/testimg.jpeg'),
                   ),
                 ),
               ),
-              Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.red.withAlpha(80),
-                          borderRadius: BorderRadius.circular(18)),
-                      margin: const EdgeInsets.all(20),
-                      padding: const EdgeInsets.all(10),
-                      child: const Text("20,000 KIP",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold)))),
-              Positioned(
-                  top: 20,
-                  right: 20,
-                  child: Container(
-                      margin: EdgeInsets.all(20),
-                      child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isFavorite = !isFavorite!;
-                          });
-                        },
-                        icon: Icon(Icons.favorite),
-                        color: isFavorite! ? Colors.red : Colors.grey,
-                      )))
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ThirdScreen()));
-            },
-            child: const Text("ໜ້າຖັດໄປ",
-                style: TextStyle(fontFamily: 'phetsarath')),
-          ),
-        ],
-      )),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ThirdScreen()));
+              },
+              child: const Text("ໜ້າຖັດໄປ",
+                  style: TextStyle(fontFamily: 'phetsarath')),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
