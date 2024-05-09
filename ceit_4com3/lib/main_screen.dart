4fd32fccc131cf8dd06d4f4f78134edd60fa22b9
Savath Saypadith <mr.saypadith@gmail.com>
2024-05-09 09:30:24 +0700

    add container

--- ceit_4com3/lib/main_screen.dart
+++ ceit_4com3/lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +11,55 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello, World!");
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("4COM3"),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        bottomRight: Radius.circular(24)),
+                    color: Colors.green,
+                  ),
+                  child: Center(
+                      child: Animate(effects: [
+                    SlideEffect(duration: Duration(seconds: 2))
+                  ], child: Text("CEIT"))),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 200,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
