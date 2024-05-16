b30ed1722193c0d514648bb890fb9e1c4823a5b7
Savath Saypadith <mr.saypadith@gmail.com>
2024-05-16 09:54:16 +0700

    add button and image

--- ceit_4com3/lib/main_screen.dart
+++ ceit_4com3/lib/main_screen.dart
@@ -12,10 +12,11 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("4COM3"),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text("4COM3"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             Row(
               children: [
@@ -51,6 +52,12 @@ class _MainScreenState extends State<MainScreen> {
                   width: 200,
                   height: 100,
                   color: Colors.yellow,
+                  child: Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                  ),
                 ),
                 Container(
                   width: 100,
@@ -59,7 +66,40 @@ class _MainScreenState extends State<MainScreen> {
                 ),
               ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(onPressed: () {}, child: Text("Cancel")),
+                ElevatedButton(onPressed: () {}, child: Text("Accept")),
+                OutlinedButton(onPressed: () {}, child: Text("OK")),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward_ios),
+                ),
+              ],
+            ),
+            Container(
+              height: 200,
+              width: 500,
+              margin: const EdgeInsets.all(32),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  bottomRight: Radius.circular(24),
+                ),
+                color: Colors.teal,
+              ),
+              child: const Icon(
+                Icons.calendar_month,
+                size: 100,
+                color: Colors.white,
+              ),
+            ),
+            Image.network("https://picsum.photos/200/200"),
+            Image.network("https://picsum.photos/200/200?id=20")
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
