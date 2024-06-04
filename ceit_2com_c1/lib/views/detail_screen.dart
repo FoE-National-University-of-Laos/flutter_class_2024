3358e8da54f01ceb22490ae1a53eda03ce047ace
Savath Saypadith <mr.saypadith@gmail.com>
2024-06-04 18:01:52 +0700

    stateless and stateful widget

--- ceit_2com_c1/lib/views/detail_screen.dart
+++ ceit_2com_c1/lib/views/detail_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatefulWidget {
+  DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  String showText = "This is stack";
 
   @override
   Widget build(BuildContext context) {
@@ -11,37 +18,50 @@ class DetailScreen extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.all(24),
-        child: Stack(
+        child: Column(
           children: [
-            Image.network(
-              'https://picsum.photos/250?image=3',
-              width: double.infinity,
-              height: 150,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              top: 15,
-              right: 15,
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
+            Stack(
+              children: [
+                Image.network(
+                  'https://picsum.photos/250?image=3',
+                  width: double.infinity,
+                  height: 150,
+                  fit: BoxFit.cover,
                 ),
-                child: Text('\$1,450'),
-              ),
-            ),
-            Positioned(
-              bottom: 15,
-              left: 15,
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.white.withAlpha(180),
+                Positioned(
+                  top: 15,
+                  right: 15,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text('\$1,450'),
+                  ),
                 ),
-                child: Text('Lorem Ipsum Dolor Sit Amet'),
-              ),
+                Positioned(
+                  bottom: 15,
+                  left: 15,
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withAlpha(180),
+                    ),
+                    child: Text('Lorem Ipsum Dolor Sit Amet'),
+                  ),
+                ),
+              ],
             ),
+            SizedBox(height: 40),
+            Text(showText),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showText = "Hello World";
+                  });
+                },
+                child: Text('Buy Now'))
           ],
         ),
       ),
